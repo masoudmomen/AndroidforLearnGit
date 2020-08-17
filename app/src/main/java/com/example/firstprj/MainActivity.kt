00acc085868bfb237bc3ad9c24a5ee67dce42f69
masoud momen <masoudmomen@hotmail.com>
2020-08-17 13:05:16 +0430

    commit with menu not command

--- app/src/main/java/com/example/firstprj/MainActivity.kt
+++ app/src/main/java/com/example/firstprj/MainActivity.kt
@@ -2,13 +2,16 @@ package com.example.firstprj
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast.makeText(this, "hello masoud", Toast.LENGTH_SHORT).show()
     }
 
     // add new line
     // add another line
+    // add line 3
 }
